c15952ec77bc4f9ff8e29ce8afc49766ec69531f
Nghi Nguyen <nguyennghi3489@gmail.com>
2023-06-25 10:04:51 +0700

    feat: integrate with moderation service

--- client/src/comment-list.tsx
+++ client/src/comment-list.tsx
@@ -2,12 +2,24 @@ interface IComment {
   comments: any;
 }
 export const CommentList = ({ comments }: IComment) => {
+  const renderComment = (item: any) => {
+    switch (item.status) {
+      case "pending":
+        return "Comment is reviewing";
+      case "rejected":
+        return "Comment is violated";
+      case "approved":
+        return item.content;
+      default:
+        return "Comment is reviewing";
+    }
+  };
   return (
     <div>
       <ul>
         {comments.map((item: any) => (
           <li className=" ml-4  rounded-md list-disc">
-            <p className="text-xs">{item.content}</p>
+            <p className="text-xs">{renderComment(item)}</p>
           </li>
         ))}
       </ul>
---
+++ services/moderation/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentCreated") {
+    const status = data.content.includes("orange") ? "rejected" : "approved";
+
+    axios.post("http://localhost:4005/events", {
+      type: "CommentModerated",
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+  res.send({});
+});
+
+app.listen(4003, () => {
+  console.log("listening on 4003");
+});
