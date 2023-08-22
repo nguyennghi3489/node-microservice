dc5fa240854b3b0630f9de095ff382b0302a80c5
Nghi Nguyen <nguyennghi3489@gmail.com>
2023-08-22 20:31:23 +0700

    chore: restructure client folder

--- services/client/src/comment.tsx
+++ services/client/src/comment.tsx
@@ -28,6 +28,7 @@ export const Comment = ({ postId, comments }: IComment) => {
               type="text"
               name="first-name"
               id="first-name"
+              placeholder="Your comment"
               onChange={(e) => setContent(e.target.value)}
               className="p-4 block w-full rounded-md border-0 py-1.5 text-gray-900  ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             />
@@ -37,7 +38,7 @@ export const Comment = ({ postId, comments }: IComment) => {
             type="submit"
             className=" rounded-md text-indigo-600 px-3 py-1 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
-            Add Comment
+            Put a Comments
           </button>
         </div>
       </form>
